ba5689e5d1e4bf811042b4d77bfd09ed89664c2d
christianpaez <christianpaeznegron@hotmail.com>
2025-12-08 01:53:02 -0500

    change to user variable in bash

--- bin/image_copy.sh
+++ bin/image_copy.sh
@@ -7,6 +7,6 @@ TEMP="/tmp/unzip_$$"
 mkdir -p "$TEMP"
 unzip -q "$ZIP" -d "$TEMP"
 find "$TEMP" -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" | head -1 | while read -r IMG; do
-    mv "$IMG" "/home/christianpaez/Documents/day-portfolio/assets/img/${NAME}.${IMG##*.}"
+    mv "$IMG" "/home/${USER}/Documents/day-portfolio/assets/img/${NAME}.${IMG##*.}"
 done
 rm -rf "$TEMP"
